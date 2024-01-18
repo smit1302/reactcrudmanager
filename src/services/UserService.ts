1e6657224acaf4bad211bc135ad5dc548a2d4705
smit1302 <smitjchoksi28@gmail.com>
2024-01-18 18:57:55 +0530

    done

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 export class UserService{
+    static getUserById(id: number) {
+      throw new Error("Method not implemented.")
+    }
     private static URL:string='https://jsonplaceholder.typicode.com/'
     public static getAllUsers(){
         let UserURL:string = `${this.URL}/users`
